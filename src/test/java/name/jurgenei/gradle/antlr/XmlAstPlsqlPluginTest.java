63705d720d95d84c4cd3e75a93fea66f5e462e07
Jurgen Hildebrand <Jurgen.Hildebrand@ing.com>
2026-06-02 15:50:32 +0200

    Fix SpotBugs test assertion in PL/SQL plugin tests

--- src/test/java/name/jurgenei/gradle/antlr/XmlAstPlsqlPluginTest.java
+++ src/test/java/name/jurgenei/gradle/antlr/XmlAstPlsqlPluginTest.java
@@ -1,7 +1,5 @@
-package name.jurgenei.grammars.plsql;
+package name.jurgenei.gradle.antlr;
 
-import name.jurgenei.gradle.antlr.XmlAstPlsqlGradleTask;
-import name.jurgenei.gradle.antlr.XmlAstPlsqlPlugin;
 import org.gradle.api.Project;
 import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.Assert;
@@ -16,7 +14,9 @@ public class XmlAstPlsqlPluginTest {
 
         new XmlAstPlsqlPlugin().apply(project);
 
-        Assert.assertTrue(project.getTasks().getByName("plsqlXmlAst") instanceof XmlAstPlsqlGradleTask);
+        final Object task = project.getTasks().getByName("plsqlXmlAst");
+        Assert.assertNotNull(task);
+        XmlAstPlsqlGradleTask.class.cast(task);
     }
 
     @Test
@@ -26,7 +26,7 @@ public class XmlAstPlsqlPluginTest {
 
         new XmlAstPlsqlPlugin().apply(project);
 
-        final XmlAstPlsqlGradleTask task = (XmlAstPlsqlGradleTask) project.getTasks().getByName("plsqlXmlAst");
+        final XmlAstPlsqlGradleTask task = XmlAstPlsqlGradleTask.class.cast(project.getTasks().getByName("plsqlXmlAst"));
         Assert.assertEquals("plsql", task.getGrammar().get());
         Assert.assertEquals("name.jurgenei.parsers.PlSqlParser", task.getParserClassName().get());
         Assert.assertEquals("name.jurgenei.parsers.PlSqlLexer", task.getLexerClassName().get());
