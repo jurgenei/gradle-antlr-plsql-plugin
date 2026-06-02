ba32bf7e61eef90c0a68149f101da8f8d21d9365
Jurgen Hildebrand <ei@xs4all.nl>
2026-06-02 20:54:57 +0200

    Release/v0.1.2 (#4)
    * version bump 0.1.2
    
    * ignore generated-src
    
    * added comment header
    
    ---------
    
    Co-authored-by: Jurgen Hildebrand <Jurgen.Hildebrand@ing.com>

--- src/main/java/name/jurgenei/grammars/plsql/PlsqlGrammarPlugin.java
+++ src/main/java/name/jurgenei/grammars/plsql/PlsqlGrammarPlugin.java
@@ -7,6 +7,13 @@ import org.gradle.api.Project;
  * Marker Gradle plugin used to validate publication metadata for this module.
  */
 public final class PlsqlGrammarPlugin implements Plugin<Project> {
+
+    /**
+     * Creates the marker plugin instance.
+     */
+    public PlsqlGrammarPlugin() {
+    }
+
     @Override
     public void apply(Project project) {
         // Intentionally no-op: this module primarily publishes grammar artifacts.
